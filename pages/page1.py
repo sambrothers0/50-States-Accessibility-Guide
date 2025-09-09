880f8479c5e9f4e0d8d9a55c596f7d09574578f6
Sam Brothers <samjbrothers@icloud.com>
2025-09-09 15:25:20 -0400

    aesthetic changes

--- pages/page1.py
+++ pages/page1.py
@@ -5,7 +5,7 @@ import pandas as pd
 import plotly.express as px
 
 
-dash.register_page(__name__, path="/page1", name="Page 1")
+dash.register_page(__name__, path="/", name="Map")
 
 #Load the dataset
 Data_Path = Path(__file__).resolve().parent.parent / "data" / "DisabilityRankedStates.csv"
@@ -29,7 +29,14 @@ fig = px.choropleth(
 
 
 layout = html.Div([
-    #top row
-    html.Div("Ranking of how disability friendly each state is", className="block"),
+    html.Div([
+        html.H3("Mapping Accessibility Through Aggregated Scores", className="map-header"),
+        html.P("This map displays the disability friendliness of each US state, ranked numerically by points. "
+               "The ranking is aggregated from scores on various factors such as housing, healthcare, employment opportunities, "
+               "and social inclusion for people with disabilities. States with higher points are considered more "
+               "disability-friendly, while those with lower points may have more challenges in providing adequate "
+               "support and resources for individuals with disabilities. Data is sourced from AAA State of Play rankings.",
+               className="map-subheader")
+    ]),
     dcc.Graph(figure=fig)
 ])
