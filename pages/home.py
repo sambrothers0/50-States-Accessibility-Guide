c01aba037931e7c80f3fc1b0f361fca870c26784
Sam Brothers <samjbrothers@icloud.com>
2025-09-09 12:51:42 -0400

    home page updates

--- pages/home.py
+++ pages/home.py
@@ -1,7 +1,7 @@
 import dash
 from dash import html
 
-dash.register_page(__name__, path="/", name = "Home")
+dash.register_page(__name__, path="/", name = "map", title="Home")
 
 layout = html.Div([
     html.H2("Welcome to my Home Page"),
